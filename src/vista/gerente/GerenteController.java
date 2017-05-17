bb896bac5cff0d622b3db259e4ac6c6d2b1694f2
Daniel <Daniel@Dani>
2017-05-17 19:56:06 +0200

    Tooltip gerente instalados.

--- src/vista/gerente/GerenteController.java
+++ src/vista/gerente/GerenteController.java
@@ -11,6 +11,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tooltip;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import vista.Empleado.EmpleadoController;
@@ -55,21 +57,59 @@ public class GerenteController implements Initializable {
         pn_personal.setVisible(false);
         cargarTooltips();
     }
-    
-    public void cargarTooltips(){
+
+    public void cargarTooltips() {
+        Tooltip tt_personal, tt_contratar, tt_despedir, tt_atras, tt_incidencias
+                ,tt_productos, tt_tienda;
+
+//        Image image = new Image("/vista/gerente/images/inicio2.png");
+//        ImageView imageView = new ImageView(image);
+//        Tooltip tooltip = new Tooltip();
+//        tooltip.setGraphic(imageView);
+//        bt_personal.setTooltip(tooltip);
+        
+        tt_incidencias = new Tooltip("Incidencias");
+       tt_incidencias.setStyle("-fx-background-color:hsl(0, 0%, 40%);"
+               + " -fx-text-fill:white; -fx-font-size:16px;");
+       Tooltip.install(bt_incidencias, tt_incidencias);
+       
+        tt_productos = new Tooltip("Productos");
+       tt_productos.setStyle("-fx-background-color:hsl(0, 0%, 40%);"
+               + " -fx-text-fill:white; -fx-font-size:16px;");
+       Tooltip.install(bt_productos, tt_productos);
+       
+        tt_tienda = new Tooltip("Tienda");
+       tt_tienda.setStyle("-fx-background-color:hsl(0, 0%, 40%);"
+               + " -fx-text-fill:white; -fx-font-size:16px;");
+       Tooltip.install(bt_tienda, tt_tienda);
+
+       tt_contratar = new Tooltip("Contratar trabajador");
+       tt_contratar.setStyle("-fx-background-color:hsl(0, 0%, 40%);"
+               + " -fx-text-fill:white; -fx-font-size:16px;");
+       Tooltip.install(bt_contratarPersonal, tt_contratar);
+       
+       tt_despedir = new Tooltip("Despedir trabajador");
+       tt_despedir.setStyle("-fx-background-color:hsl(0, 0%, 40%);"
+               + " -fx-text-fill:white; -fx-font-size:16px;");
+       Tooltip.install(bt_despedirPersonal, tt_despedir);
+       
+       tt_atras = new Tooltip("Volver");
+       tt_atras.setStyle("-fx-background-color:hsl(0, 0%, 40%);"
+               + " -fx-text-fill:white; -fx-font-size:16px;");
+       Tooltip.install(bt_atras, tt_atras);
         
     }
 
     @FXML
     private void trabajadoresAction(ActionEvent event) {
 
-        if (bt_personal.isFocused()) {            
-            pn_inicio.setVisible(false);            
+        if (bt_personal.isFocused()) {
+            pn_inicio.setVisible(false);
             pn_personal.setVisible(true);
         }
-        
-        if(bt_atras.isFocused()){            
-            pn_inicio.setVisible(true);            
+
+        if (bt_atras.isFocused()) {
+            pn_inicio.setVisible(true);
             pn_personal.setVisible(false);
         }
 
