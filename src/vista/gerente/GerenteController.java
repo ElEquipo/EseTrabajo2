c1a00748aad6c25fce46a6c723c484996c7bcb1d
Daniel <Daniel@Dani>
2017-05-19 18:29:07 +0200

    Implimentando contratación de trabajador.

--- src/vista/gerente/GerenteController.java
+++ src/vista/gerente/GerenteController.java
@@ -6,13 +6,13 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import vista.Empleado.EmpleadoController;
@@ -34,8 +34,6 @@ public class GerenteController implements Initializable {
     @FXML
     private Button bt_cerrarSesion;
     @FXML
-    private Pane pn_personal;
-    @FXML
     private Button bt_despedirPersonal;
     @FXML
     private Button bt_contratarPersonal;
@@ -51,10 +49,43 @@ public class GerenteController implements Initializable {
     private Button bt_personal;
     @FXML
     private Pane pn_inicio;
+    @FXML
+    private TextField tf_id;
+    @FXML
+    private TextField tf_nombre;
+    @FXML
+    private TextField tf_apellido1;
+    @FXML
+    private TextField tf_apellido2;
+    @FXML
+    private TextField tf_puesto;
+    @FXML
+    private TextField tf_salario;
+    @FXML
+    private TextField tf_fecha;
+    @FXML
+    private TextField tf_nick;
+    @FXML
+    private TextField tf_pass;
+    @FXML
+    private TextField tf_horaEntrada;
+    @FXML
+    private TextField tf_horaSalida;
+    @FXML
+    private TextField tf_tienda;
+    @FXML
+    private TextField tf_dni;
+    @FXML
+    private Button bt_contratar;
+    @FXML
+    private Pane pn_menuTrabajadores;
+    @FXML
+    private Pane pn_contratar;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        pn_personal.setVisible(false);
+        pn_menuTrabajadores.setVisible(false);
+        pn_contratar.setVisible(false);
         cargarTooltips();
     }
 
@@ -62,6 +93,7 @@ public class GerenteController implements Initializable {
         Tooltip tt_personal, tt_contratar, tt_despedir, tt_atras, tt_incidencias
                 ,tt_productos, tt_tienda;
 
+        // ESTO DEJALO AQUÍ Y TE LO EXOPLICARÉ EL LUNES
 //        Image image = new Image("/vista/gerente/images/inicio2.png");
 //        ImageView imageView = new ImageView(image);
 //        Tooltip tooltip = new Tooltip();
@@ -105,12 +137,12 @@ public class GerenteController implements Initializable {
 
         if (bt_personal.isFocused()) {
             pn_inicio.setVisible(false);
-            pn_personal.setVisible(true);
+            pn_menuTrabajadores.setVisible(true);
         }
 
         if (bt_atras.isFocused()) {
             pn_inicio.setVisible(true);
-            pn_personal.setVisible(false);
+            pn_menuTrabajadores.setVisible(false);
         }
 
     }
@@ -123,6 +155,39 @@ public class GerenteController implements Initializable {
         } catch (IOException ex) {
             Logger.getLogger(EmpleadoController.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    @FXML
+    private void contratarAction(ActionEvent event) {
+        Object evento = event.getSource();
+        if(evento == bt_contratarPersonal){
+            pn_menuTrabajadores.setVisible(false);
+            pn_contratar.setVisible(true);
+        }
+    }
+
+    @FXML
+    private void inicioAction(ActionEvent event) {
+        pn_contratar.setVisible(false);
+        pn_menuTrabajadores.setVisible(false);
+        pn_inicio.setVisible(true);
+        limpiarCampos();
+    }
+    
+    public void limpiarCampos(){
+        tf_id.clear();
+        tf_dni.clear();
+        tf_nombre.clear();
+        tf_apellido1.clear();
+        tf_apellido2.clear();
+        tf_puesto.clear();
+        tf_salario.clear();
+        tf_fecha.clear();
+        tf_nick.clear();
+        tf_pass.clear();
+        tf_horaEntrada.clear();
+        tf_horaSalida.clear();
+        tf_tienda.clear();
     }
 
 }
